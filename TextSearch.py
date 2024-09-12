daf1cc60c3a03b41df650fdd9fa2646caab1d58f
leducthanhkim2004 <10422105@student.vgu.edu.vn>
2024-09-12 23:48:55 +0700

    update

--- TextSearch.py
+++ TextSearch.py
@@ -5,6 +5,10 @@ import json
 import torch
 
 class textSearch:
+    """
+    This class contain all function for retrievealing image based on queries 
+    """
+    
     def __init__(self, clipv2_bin: str, keyframe_file_json: str):
         self.clip_index = self.load_bin_file(clipv2_bin)
         self.id2img_fps = self.load_json_file(keyframe_file_json)
@@ -34,22 +38,10 @@ class textSearch:
         text_features /= text_features.norm(dim=-1, keepdim=True)
         text_features = text_features.cpu().detach().numpy().astype(np.float32)
 
-        if index is None:
-            scores, index_image = self.clip_index.search(text_features, k=k)
-        else:
-            id_selector = faiss.IDSelectorArray(index)
-            scores, index_image = self.clip_index.search(text_features, k=k, params=faiss.SearchParametersIVF(id_selector))
-
+        
+        scores, index_image = self.clip_index.search(text_features, k=k)
+        
         idx_image = index_image.flatten()
         infos_query = [self.find_video_info(idx) for idx in idx_image]  # Ensure idx is a valid index
         link_paths= [info["image_path"] for info in infos_query]
         return scores.flatten(), idx_image,link_paths
-link_bin_file = r"C:\Users\leduc\ai\data\faiss_clipv2_cosine.bin"
-json_file = r"C:\Users\leduc\ai\data\id2img_fps.json"
-text_searcher = textSearch(link_bin_file, json_file)
-input_test = "the women is wearing a red skirt "
-scores, idx_image , image_path= text_searcher.text_search(input_test, k=10, index=None)  # Ensure all three values are returned
-
-print("Image Indexes:", idx_image)
-
-print("Image path ", image_path)
